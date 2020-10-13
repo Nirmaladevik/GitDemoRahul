2251a24f3310f01acf39f7b32fa839c22dac8ccf
Nirmala <nirmalauk81@gmail.com>
2020-10-13 13:01:28 +0100

    first commit

---
+++ src/test/java/rahulshettyacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+
+		@Test
+		public void android()
+		{
+		System.out.println("android");	
+		}
+		
+		@Test
+		public void IOSapp()
+		{
+		System.out.println("IOSapp");	
+		}
+}
+
---
+++ src/test/java/rahulshettyacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+
+	@Test
+	public void day1()
+	{
+	System.out.println("day1");	
+	}
+	
+	@Test
+	public void day2()
+	{
+	System.out.println("day2");	
+	}
+}
